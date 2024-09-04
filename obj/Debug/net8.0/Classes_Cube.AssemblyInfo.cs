d8bb690041f0be169a1431f78ddce0e278813ecc
software@gelti.fr <software@gelti.fr>
2024-09-04 15:30:11 +0200

    update

--- obj/Debug/net8.0/Classes_Cube.AssemblyInfo.cs
+++ obj/Debug/net8.0/Classes_Cube.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Classes_Cube")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ae1f36ec4fa1927aa6ca94adb92b056c041f52f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+966c17aa0190415508a6dba6fc69b30bdd57349b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Classes_Cube")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Classes_Cube")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
